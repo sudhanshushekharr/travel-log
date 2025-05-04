95bee85043acb6243261dc1f33a561e7d2301736
Shekhar <shekharsudhanshu801@gmail.com>
2025-05-04 17:19:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+
+
+#log entry
+
+
+*title-text
+*description-text
+*comments-text
+*rating-scale of 1-10
+*image-text-URL
+*start date -DateTime
+*end date -DateTime
+*latitude-Number
+*longitude-Number
+*created at-datetime
+*updated at-datetime
+
+
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express= require('express')
+const morgan=require('morgan');
+const helmet=require('helmet');
+const cors=require('cors')
+
+const mongoose=require('mongoose');
+
+require('dotenv').config();
+
+
+const middlewares=require('./middlewares');
+const logs=require('./api/logs')
+
+
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const app=express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors(
+    {
+        origin: process.env.CORS_ORIGIN,
+    }
+));
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+res.json(
+    {
+        message:'Hello world',
+    }
+)
+})
+
+app.use('/api/logs',logs);
+
+app.use(middlewares.notFound);
+
+
+//error handling
+
+app.use(middlewares.errorHandler)
+
+const port=process.env.PORT || 1337;
+
+app.listen(port, ()=>{
+    console.log('Listening at http://localhost:1337');
+    
+});
+
